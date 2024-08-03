2e15eb6317b0ed508e8ceae85abf182e4930cfd1
Georg Sieber <it@georg-sieber.de>
2024-08-03 20:43:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# G729lib
+Python bindings for the libbcg729 audio codec C library.
+
+Make sure you have the Ubuntu/Debian package `libbcg729-0` installed.
---
+++ g729lib/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+G729 codec encoder/decoder Python module
+~~~~~~~
+
+Python bindings for the libbcg729 audio codec C library.
+
+:source: <https://github.com/schorschii/g729lib>
+"""
+
+from .api import Encoder, Decoder  # NOQA
+
+__author__    = 'Georg Sieber'
+__copyright__ = 'Copyright (c) 2024'
+__license__   = 'GPL-3.0'
